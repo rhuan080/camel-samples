3d673c6bb04e6590690ef35a6b1818235cb1b3ca
Rhuan Rocha <rhuan080@gmail.com>
2023-02-21 18:53:50 -0300

    Update App.java

--- restsample/src/main/java/net/rhuanrocha/App.java
+++ restsample/src/main/java/net/rhuanrocha/App.java
@@ -10,7 +10,6 @@ public class App
 {
     public static void main( String[] args ) throws Exception {
         Main main = new Main(App.class);
-        //main.getCamelContext().addRoutes(new RestRoute());
         main.run(args);
     }
 }
